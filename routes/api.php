7bf2da63cac2a9bfa95d09d575359007c83114ff
tripgrass <wjgrass@gmail.com>
2024-10-11 09:15:02 -0700

    add use UserCpntroller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
