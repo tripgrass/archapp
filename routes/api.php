52ee3777581f5cf91a8f39545e2e6a94d04e80de
tripgrass <wjgrass@gmail.com>
2024-10-13 18:01:25 -0700

    add namespace

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ArtifactController;
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
