60f04bd699b9c932e516cd77019bb4340e38c029
tripgrass <wjgrass@gmail.com>
2024-10-13 21:10:34 -0700

    update client oauth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        Passport::enablePasswordGrant();        
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['oauth/*', 'sanctum/csrf-cookie', 'api/*],
 
     'allowed_methods' => ['*'],
 
