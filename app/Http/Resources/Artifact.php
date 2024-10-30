5a213d7aefc7b164644147ce3c288148ce7d4d92
tripgrass <wjgrass@gmail.com>
2024-10-30 10:54:52 -0700

    update singlre artifact resource

--- app/Http/Resources/Artifact.php
+++ app/Http/Resources/Artifact.php
@@ -18,6 +18,13 @@ class Artifact extends JsonResource
         return [
             'id'         => $this->id,
             'name'       => $this->name,
+            'location'   => $this->location,
+            'area'   => $this->area,
+            'address'   => $this->address,
+            'city'   => $this->city,
+            'state'   => $this->state,
+            'zipcode'   => $this->zipcode,
+            'eloGroup'   => $this->eloGroup,            
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];        
