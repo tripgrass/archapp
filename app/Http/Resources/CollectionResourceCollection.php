b9e0ff36cffed39f2e0c907fe6768bc38d9f3c5b
tripgrass <wjgrass@gmail.com>
2024-12-16 08:36:37 -0700

    add collections

--- app/Http/Resources/CollectionResourceCollection.php
+++ app/Http/Resources/CollectionResourceCollection.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use App\Models\Image;
 
 class CollectionResourceCollection extends ResourceCollection
 {
