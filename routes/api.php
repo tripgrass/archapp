26ef87b6d563f6b46dbaef87273cadc86cdcdeb9
tripgrass <wjgrass@gmail.com>
2024-12-16 08:52:47 -0700

    add collections

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\ApiArtifactController;
+use App\Http\Controllers\Api\ApiCollectionController;
 use App\Http\Controllers\Api\ApiImageController;
 use App\Http\Controllers\Api\ApiPersonController;
 use App\Http\Controllers\Api\ApiUserController;
