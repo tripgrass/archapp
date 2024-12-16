0d0570daeaff359332c6d35c76b1874b8d8b24ce
tripgrass <wjgrass@gmail.com>
2024-12-16 10:27:22 -0700

    add softdelete

--- app/Http/Resources/CollectionResourceCollection.php
+++ app/Http/Resources/CollectionResourceCollection.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use App\Models\Image;
 
-class ArtifactCollection extends ResourceCollection
+class CollectionResourceCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
