a29f573c8397cd578abc7b43223857efb7b9edcb
tripgrass <wjgrass@gmail.com>
2024-12-16 10:28:51 -0700

    add softdelete

--- app/Http/Resources/CollectionResource.php
+++ app/Http/Resources/CollectionResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Artifact extends JsonResource
+class CollectionResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
