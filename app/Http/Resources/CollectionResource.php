038d2b197b425d67d91d5d8fef9378146d857c8f
tripgrass <wjgrass@gmail.com>
2024-12-16 10:31:22 -0700

    add softdelete

--- app/Http/Resources/CollectionResource.php
+++ app/Http/Resources/CollectionResource.php
@@ -20,24 +20,7 @@ class CollectionResource extends JsonResource
         }
         return [
             'id'         => $this->id,
-            'name'       => $this->name,
-            'location'   => $this->location,
-            'latitude' => $this->location ? $this->location->latitude : null,
-            'longitude' => $this->location ? $this->location->longitude : null,            
-            'area'   => $this->area,
-            'address'   => $this->address,
-            'city'   => $this->city,
-            'state'   => $this->state,
-            'zipcode'   => $this->zipcode,
-            'eloGroup'   => $this->eloGroup,            
-            'scale'   =>    $this->scale,            
-            'grade'   =>    $this->grade,            
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'images'     => $this->images,
-            'initial_year'     => $this->initial_year,            
-            'description'     => $this->description,            
-            'primary_image_id' => $this->primary_image_id
+            'name'       => $this->name
         ];        
     }
 }
